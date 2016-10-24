ea1f8dd3574a7be13f60511d1c4cc902d3b0c7b1
hwangyunju <hwang_yun@Naver.com>
2016-10-24 12:53:18 +0900

    Update merge.c

--- merge.c
+++ merge.c
@@ -4,6 +4,7 @@
 #include <sys/time.h>
 
 int readaline_and_out(FILE *fin, FILE *fout);
+void reverse(char str[], int size);
 
 int
 main(int argc, char *argv[])
@@ -80,9 +81,23 @@ readaline_and_out(FILE *fin, FILE *fout)
                 break;
             }
         }
-        fputc(ch, fout);
+  //    fputc(ch, fout);
+        arr[count]=ch;
         count++;
     } while (ch != 0x0a);
+    reverse(arr,count);
+    fputs(arr,fout);
     return 0;
+}
+
+void reverse(char str[],int size)
+{
+    int i;
+    char temp;
+    for(i=0;i<size/2;i++){
+        temp = str[i];
+        str[i] = str[size-1-i];
+        str[size-1-i]=temp;
+    }
 }
 
